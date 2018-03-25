7378ca0976f375ddf992dce7e01a9937139d2267
fricha05 <fricha05@etud.u-pem.fr>
2018-03-25 14:55:13 +0200

    js

---
+++ Rework Portfolio/lib/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+        anchors: ['page1', 'page2', 'page3'],
+        navigation: true
+    });
+
+    $('.nav ul a').on('click', function () {
+        $('.nav ul a').removeClass('active');
+        $(this).addClass('active');
+    });
+
+
+    $(window).bind('mousewheel', function (e) {
+        $('.nav li a').each(function(){
+            var currLink = window.location.href;
+            var refElement = 'http://127.0.0.1:56865/Rework%20Portfolio/index.html' + $(this).attr("href");
+            if(currLink === refElement){
+                $(this).addClass('active');
+            }
+            else{
+                $(this).removeClass('active');
+            }
+        });
+    });
+    
+    $(window).bind('mousewheel', function (e) {
+        $('.nav li a').each(function(){
+            var currLink = window.location.href;
+            var refElement = 'http://127.0.0.1:56865/Rework%20Portfolio/index.html' + $(this).attr("href");
+            if(currLink === refElement){
+                $(this).addClass('active');
+            }
+            else{
+                $(this).removeClass('active');
+            }
+        });
+    });
+});
